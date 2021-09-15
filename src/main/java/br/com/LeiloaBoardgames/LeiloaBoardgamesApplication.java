bc93d114977969326067a3a10ee41ab5354cead0
Julio Barcellos <46910743+juliobarcellos@users.noreply.github.com>
2021-09-15 01:11:37 -0300

    Esqueleto do projeto
    Adicionando arquivos base do projeto

---
+++ src/main/java/br/com/LeiloaBoardgames/LeiloaBoardgamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.LeiloaBoardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiloaBoardgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiloaBoardgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LeiloaBoardgames/LeiloaBoardgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LeiloaBoardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiloaBoardgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
