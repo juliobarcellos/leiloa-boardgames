3300979e668a609f0ab948bda39facaa643d2982
Roney Cacau <roneycacau@gmail.com>
2021-11-11 02:11:46 -0300

    feat - add modelMapper

--- src/main/java/br/com/LeiloaBoardgames/LeiloaBoardgamesApplication.java
+++ src/main/java/br/com/LeiloaBoardgames/LeiloaBoardgamesApplication.java
@@ -1,10 +1,17 @@
 package br.com.LeiloaBoardgames;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeiloaBoardgamesApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeiloaBoardgamesApplication.class, args);
