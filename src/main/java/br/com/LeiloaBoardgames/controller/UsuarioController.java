eee6e36c18869087f01bd9c5d99bdf23e9f32980
Roney Cacau <roneycacau@gmail.com>
2022-01-04 17:11:08 -0300

    feat - unit tests

--- src/main/java/br/com/LeiloaBoardgames/controller/UsuarioController.java
+++ src/main/java/br/com/LeiloaBoardgames/controller/UsuarioController.java
@@ -3,13 +3,16 @@ package br.com.LeiloaBoardgames.controller;
 import br.com.LeiloaBoardgames.domain.Usuario;
 import br.com.LeiloaBoardgames.domain.request.UsuarioRequest;
 import br.com.LeiloaBoardgames.domain.response.UsuarioResponse;
+import br.com.LeiloaBoardgames.exceptions.ApiErrors;
 import br.com.LeiloaBoardgames.service.UsuarioService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -21,8 +24,19 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UsuarioResponse create(@RequestBody UsuarioRequest usuario) {
-        Usuario entity = mapper.map(usuario, Usuario.class);
-        return mapper.map(service.save(entity), UsuarioResponse.class);
+    public UsuarioResponse create(@Valid @RequestBody UsuarioRequest request) {
+        Usuario entity = mapper.map(request, Usuario.class);
+        entity = service.save(entity);
+        return mapper.map(entity, UsuarioResponse.class);
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors(bindingResult);
+    }
+
+
 }
--- src/test/java/br/com/LeiloaBoardgames/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/LeiloaBoardgames/controller/UsuarioControllerTest.java
@@ -4,12 +4,11 @@ import br.com.LeiloaBoardgames.domain.Usuario;
 import br.com.LeiloaBoardgames.domain.request.UsuarioRequest;
 import br.com.LeiloaBoardgames.domain.response.UsuarioResponse;
 import br.com.LeiloaBoardgames.service.UsuarioService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,6 +22,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,35 +42,22 @@ public class UsuarioControllerTest {
 
     @MockBean
     private UsuarioService service;
+    @MockBean
+    private ModelMapper mapper;
 
     @Test
     @DisplayName("Teste de cadastro de usuário")
     public void createUser() throws Exception {
 
-        UsuarioRequest usuarioRequest = new UsuarioRequest();
-        String json = new ObjectMapper().writeValueAsString(usuarioRequest);
+        UsuarioRequest usuarioRequest = getUsuarioRequestMock();
+        UsuarioResponse response = getUsuarioResponseMock();
+        Usuario usuarioSalvo = getUsuarioMock();
 
-        UsuarioResponse response = UsuarioResponse.builder()
-                .id(1)
-                .nome("zé")
-                .usuario("seuze")
-                .email("ze@email.com")
-                .senha("12345")
-                .cpf("12345678910")
-                .dataNascimento(LocalDate.parse("2000-01-01"))
-                .telefone("11912345678")
-                .build();
-        Usuario usuarioSalvo = Usuario.builder().id(1)
-                .nome("zé")
-                .usuario("seuze")
-                .email("ze@email.com")
-                .senha("12345")
-                .cpf("12345678910")
-                .dataNascimento(LocalDate.parse("2000-01-01"))
-                .telefone("11912345678")
-                .build();
+        String json = new ObjectMapper().findAndRegisterModules().writeValueAsString(usuarioRequest);
 
         when(service.save(any(Usuario.class))).thenReturn(usuarioSalvo);
+        when(mapper.map(any(UsuarioRequest.class), any())).thenReturn(usuarioSalvo);
+        when(mapper.map(any(Usuario.class), any())).thenReturn(response);
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(USER_API)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
@@ -88,6 +75,54 @@ public class UsuarioControllerTest {
     @Test
     @DisplayName("Teste de cadastro de usuário com dados inválidos")
     public void createUserWithError() throws Exception {
+        UsuarioRequest userRequest = new UsuarioRequest();
+        String json = new ObjectMapper().findAndRegisterModules().writeValueAsString(userRequest);
 
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(USER_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(7)));
+
+    }
+
+    private UsuarioRequest getUsuarioRequestMock() {
+        UsuarioRequest usuarioRequest = new UsuarioRequest();
+        usuarioRequest.setNome("zé");
+        usuarioRequest.setUsuario("seuze");
+        usuarioRequest.setEmail("ze@email.com");
+        usuarioRequest.setSenha("12345");
+        usuarioRequest.setCpf("12345678910");
+        usuarioRequest.setDataNascimento(LocalDate.parse("2000-01-01"));
+        usuarioRequest.setTelefone("11912345678");
+        return usuarioRequest;
+    }
+    
+    private Usuario getUsuarioMock() {
+        return Usuario.builder().id(1)
+                    .nome("zé")
+                    .usuario("seuze")
+                    .email("ze@email.com")
+                    .senha("12345")
+                    .cpf("12345678910")
+                    .dataNascimento(LocalDate.parse("2000-01-01"))
+                    .telefone("11912345678")
+                .build();
+    }
+
+    private UsuarioResponse getUsuarioResponseMock() {
+        return UsuarioResponse.builder()
+                .id(1)
+                .nome("zé")
+                .usuario("seuze")
+                .email("ze@email.com")
+                .senha("12345")
+                .cpf("12345678910")
+                .dataNascimento(LocalDate.parse("2000-01-01"))
+                .telefone("11912345678")
+                .build();
     }
 }
