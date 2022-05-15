ad511e5c848ef0928f30246a4c060696cb4fc67a
Roney Cacau <roneycacau@gmail.com>
2022-05-15 17:14:38 -0300

    refact - add all fields in createRequest

--- src/main/java/br/com/LeiloaBoardgames/domain/request/UsuarioCreateRequest.java
+++ src/main/java/br/com/LeiloaBoardgames/domain/request/UsuarioCreateRequest.java
@@ -1,9 +1,13 @@
 package br.com.LeiloaBoardgames.domain.request;
 
+import br.com.LeiloaBoardgames.domain.EstadoEnum;
+import br.com.LeiloaBoardgames.domain.TipoDocumentoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -34,8 +38,29 @@ public class UsuarioCreateRequest {
 
     @NotNull(message = "Data de nascimento é obrigatório")
     @Past
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataNascimento;
 
     @NotBlank(message = "Telefone é obrigatório")
     private String telefone;
+
+    @NotBlank(message = "Tipo de documento é obrigatório")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private TipoDocumentoEnum tipoDocumento;
+
+    @NotBlank(message = "Numero de documento é obrigatório")
+    private String numDocumento;
+
+    @NotBlank(message = "Orgao expedidor é obrigatório")
+    private String orgaoExpeditor;
+
+    @NotBlank(message = "Estado é obrigatório")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private EstadoEnum estado;
+
+    @NotBlank(message = "Data de expedicao é obrigatório")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataEmissao;
 }
